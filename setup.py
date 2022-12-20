4b98221be77cbf44d9f7e136463b82c88595bf6e
Darth-Kylokun <60204563+Darth-Kylokun@users.noreply.github.com>
2022-12-20 15:02:36 -0500

    Added in setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from importlib.metadata import entry_points
+
+setup(
+    name="DnD_Roller",
+    version="1.0.0",
+    author="Karsten Courtney",
+    author_email="ftwkrc@gmail.com",
+    packages=["dnd_roller"],
+    entry_points={
+        "console_scripts": [
+            "dnd_roll=dnd_roller.__main__:main"
+        ]
+    }
+)
